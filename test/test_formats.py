621c6daa10ebe9011a142283e109dba5f10de3ce
yeago <yeago999@gmail.com>
2020-07-18 20:55:11 +0000

    tests

--- test/test_formats.py
+++ test/test_formats.py
@@ -1,7 +1,16 @@
 import unittest
-from datetime import datetime
+import os
+import yaml
 
 
 class TestFormat(unittest.TestCase):
+    def setUp(self):
+        self._data = {}
+        for filename in os.listdir('./formats/mtg/'):
+            if filename[-3:] == 'yml':
+                with open('./formats/mtg/%s' % filename, 'r') as fileobj:
+                    self._data[filename.split('.')[0]] = yaml.load(fileobj.read())
+
+
     def test_format_data(self):
-        pass
+        self.assertTrue(self._data)
