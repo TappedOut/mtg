aa3516e9ac2382d53b22f06b01aeac5fc0bdc094
yeago <yeago999@gmail.com>
2020-07-18 21:04:45 +0000

    more tests

--- test/test_formats.py
+++ test/test_formats.py
@@ -1,5 +1,6 @@
 import unittest
 import os
+from slugify import slugify
 import yaml
 
 
@@ -12,5 +13,16 @@ class TestFormat(unittest.TestCase):
                     self._data[filename.split('.')[0]] = yaml.load(fileobj.read())
 
 
-    def test_format_data(self):
+    def test_format_keys(self):
+        for slug, item in self._data.items():
+            for key in item.keys():
+                assertTrue(key in ['inherits', 'sets', 'bans', 'assigns_commander_identity', 'has_sideboard',
+                                   'is_singleton', 'maximum_deck_size', 'minium_deck_size', 'starting_life_total'])
+
+    def test_inheritance(self):
+        for slug, data in format_data.items():
+            if data.get('inherits'):
+                for item in data['inherits']:
+                    self.assertTrue(slugify(item) in self._data)
+
         self.assertTrue(self._data)
